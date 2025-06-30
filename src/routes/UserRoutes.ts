85ab5b08456f0897352fe20a021f7041e9659710
DidiAndroidP <didier.mendoza.uni@gmail.com>
2025-06-29 19:17:48 -0600

    asda

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -7,7 +7,7 @@ const router = Router();
 const userController = new UserController();
 
 router.get("/get-all", userController.getAllUsers.bind(userController));
-router.get("/:id", authenticateToken,userController.getUserById.bind(userController));
+router.get("/:id",userController.getUserById.bind(userController));
 router.put("/update/:id", uploadUserImage,authenticateToken, userController.updateUser.bind(userController)); 
 router.delete("/delete/:id", userController.deleteUser.bind(userController));
 
